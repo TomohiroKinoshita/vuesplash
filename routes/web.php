b8bd0026892dbd79e38c492cd9bae2ad7ed9279b
TomohiroKinoshita <t.kinoshita922@gmail.com>
2020-07-09 05:37:00 +0900

    errors after installation for auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix')
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,22 +10,6 @@ const mix = require('laravel-mix')
  | file for the application as well as bundling up all the JS files.
  |
  */
-//成功　watchしながらphp serve  8081
-// mix.js('resources/js/app.js', 'public/js')
-//   .version()
 
-//成功　watchしながらphp serve  8081
-//valetじゃないので名前解決できないはず
-// mix.browserSync('vuesplash.test')
-//   .js('resources/js/app.js', 'public/js')
-//   .version()
-
-
-//成功　watchしながらphp serve  8081
-//watchはしていないと変更が反映されない
-  mix.browserSync({
-    proxy: '0.0.0.0:8081', // アプリの起動アドレス
-    open: false // ブラウザを自動で開かない
-  })
-  .js('resources/js/app.js', 'public/js')
-  .version()
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
