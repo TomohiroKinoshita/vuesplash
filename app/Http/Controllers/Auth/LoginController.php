8f9c26fcf8d831f25e5988934485debff5f13401
TomohiroKinoshita <t.kinoshita922@gmail.com>
2020-07-09 22:57:06 +0900

    LoginApi test done

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; // ★ 追加
 class LoginController extends Controller
 {
     /*
@@ -37,7 +37,7 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    // 追加
+    // // ★ メソッド追加
     protected function authenticated(Request $request, $user)
     {
         return $user;
