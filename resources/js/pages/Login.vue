2d990588fc879f2049363806f6572d35f6a7baaf
TomohiroKinoshita <t.kinoshita922@gmail.com>
2020-07-11 04:11:00 +0900

    会員登録実装完了

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -12,32 +12,36 @@
     @click="tab = 2"
   >Register</li>
 </ul>
-    <div class="panel" v-show="tab === 1"><div class="panel" v-show="tab === 1">
-    <form class="form" @submit.prevent="login"> 
-    <label for="login-email">Email</label>
-    <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
-    <label for="login-password">Password</label>
-    <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
-    <div class="form__button">
-      <button type="submit" class="button button--inverse">login</button>
+    <div class="panel" v-show="tab === 1">
+      <div class="panel" v-show="tab === 1">
+        <form class="form" @submit.prevent="login"> 
+          <label for="login-email">Email</label>
+          <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+          <label for="login-password">Password</label>
+          <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+          <div class="form__button">
+            <button type="submit" class="button button--inverse">login</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </form>
-</div></div>
-<div class="panel" v-show="tab === 2"><div class="panel" v-show="tab === 2">
-  <form class="form" @submit.prevent="register">
-    <label for="username">Name</label>
-    <input type="text" class="form__item" id="username" v-model="registerForm.name">
-    <label for="email">Email</label>
-    <input type="text" class="form__item" id="email" v-model="registerForm.email">
-    <label for="password">Password</label>
-    <input type="password" class="form__item" id="password" v-model="registerForm.password">
-    <label for="password-confirmation">Password (confirm)</label>
-    <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
-    <div class="form__button">
-      <button type="submit" class="button button--inverse">register</button>
-    </div>
-  </form>
-</div></div>
+<div class="panel" v-show="tab === 2">
+  <div class="panel" v-show="tab === 2">
+    <form class="form" @submit.prevent="register">
+      <label for="username">Name</label>
+      <input type="text" class="form__item" id="username" v-model="registerForm.name">
+      <label for="email">Email</label>
+      <input type="text" class="form__item" id="email" v-model="registerForm.email">
+      <label for="password">Password</label>
+      <input type="password" class="form__item" id="password" v-model="registerForm.password">
+      <label for="password-confirmation">Password (confirm)</label>
+      <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+      <div class="form__button">
+        <button type="submit" class="button button--inverse">register</button>
+      </div>
+    </form>
+  </div>
+</div>
   </div>
 </template>
 
@@ -59,17 +63,24 @@ export default {
       
     }
   },
-      methods: {
+  methods: {
     login () {
       console.log(this.loginForm)
-    }
+    }   
   },
-      methods: {
-      login () {/* 中略 */},
-      register () {
-        console.log(this.registerForm)
-      }
-    }
+  methods: {
+
+  login () {/* 中略 */},
+
+  async register () {
+
+    // authストアのresigterアクションを呼び出す
+    await this.$store.dispatch('auth/register', this.registerForm)
+
+    // トップページに移動する
+    this.$router.push('/')
+  }
+}
 
 }
 </script>
