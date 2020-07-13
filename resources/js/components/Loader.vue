b544f6774de94a9a788dc08c7efd3ae70b310cf8
TomohiroKinoshita <t.kinoshita922@gmail.com>
2020-07-14 05:09:57 +0900

    ローダーコンポーネント作成

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="loader">
+        <p class="loadeing__text">
+            <slot>Loading...</slot>
+        </p>
+        <div class="loader__item loader__item--heart">
+            <div></div>
+        </div>
+    </div>
+</template>
+
+    
+
